22daca62a722517d3611f04fafecd279499fba4f
pikesley <github@orgraphone.org>
2014-12-13 16:35:41 +0000

    Ooh, tidy

--- lib/wordbot/bot.rb
+++ lib/wordbot/bot.rb
@@ -1,7 +1,37 @@
 module Wordbot
   class Bot
     def self.split string
-      string.split /([ ])/
+      compress string.split /([ \?])/
+    end
+
+    def self.compress list
+      a = []
+      buffer = ''
+      list.each do |i|
+        if is_word i
+          unless buffer == ''
+            a.push buffer
+            buffer = ''
+          end
+          a.push i
+        else
+          buffer << i
+        end
+      end
+      unless buffer == ''
+        a.push buffer
+      end
+
+      a
+    end
+
+    def self.is_word string
+      return false if string == ''
+      string.split('').all? { |c| is_letter c }
+    end
+
+    def self.is_letter char
+      'abcdefghijklmnopqrstuvwxyz'.index char.downcase
     end
   end
 end
--- spec/wordbot/wordbot_spec.rb
+++ spec/wordbot/wordbot_spec.rb
@@ -15,7 +15,29 @@ module Wordbot
         expect(Wordbot::Bot.split 'aa bb').to eq ['aa', ' ', 'bb']
       end
 
-      it 'should split'
+      it 'should split "aa? bb"' do
+        expect(Wordbot::Bot.split 'aa? bb').to eq ['aa', '? ', 'bb']
+      end
+    end
+
+    context 'Compress some strings' do
+      it 'should join up the non-word content' do
+        expect(Wordbot::Bot.compress ['aa', '?', '', ' ', 'bb']).to eq [
+          'aa',
+          '? ',
+          'bb'
+        ]
+      end
+    end
+
+    context 'Detect a word' do
+      it 'should recognise a word' do
+        expect(Wordbot::Bot.is_word 'aa').to eq true
+        expect(Wordbot::Bot.is_word 'abc').to eq true
+        expect(Wordbot::Bot.is_word '123').to eq false
+        expect(Wordbot::Bot.is_word '?').to eq false
+        expect(Wordbot::Bot.is_word '').to eq false
+      end
     end
   end
 end
