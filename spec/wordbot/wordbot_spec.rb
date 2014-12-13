bab142a7b9129d351bbec41be477eb175e932a42
pikesley <github@orgraphone.org>
2014-12-13 15:43:48 +0000

    I think this is all the simple cases

--- spec/wordbot/wordbot_spec.rb
+++ spec/wordbot/wordbot_spec.rb
@@ -3,9 +3,19 @@ require 'spec_helper'
 module Wordbot
   describe Bot do
     context 'Split some strings' do
-      it 'should split the simplest string' do
+      it 'should split "a"' do
         expect(Wordbot::Bot.split 'a').to eq ['a']
       end
+
+      it 'should split "a "' do
+        expect(Wordbot::Bot.split 'a ').to eq ['a', ' ']
+      end
+
+      it 'should split " a "' do
+        expect(Wordbot::Bot.split ' a ').to eq [' ', 'a', ' ']
+      end
+
+      it 'should'
     end
   end
 end
