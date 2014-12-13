4693c5028d76e32bd971a7f6786e20e0b3115793
pikesley <github@orgraphone.org>
2014-12-13 15:55:08 +0000

    Nope, irrelevant

--- spec/wordbot/wordbot_spec.rb
+++ spec/wordbot/wordbot_spec.rb
@@ -11,11 +11,11 @@ module Wordbot
         expect(Wordbot::Bot.split 'a ').to eq ['a', ' ']
       end
 
-      it 'should split " a "' do
-        expect(Wordbot::Bot.split ' a ').to eq [' ', 'a', ' ']
+      it 'should split "aa bb"' do
+        expect(Wordbot::Bot.split 'aa bb').to eq ['aa', ' ', 'bb']
       end
 
-      it 'should'
+      it 'should split'
     end
   end
 end
